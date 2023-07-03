07c7b14bf98771b3e69a160c74d5c3333a120987
Angelo Filho <angelo.filho06@hotmail.com>
2023-07-03 13:30:02 -0300

    feat: add expenses list widget

--- lib/screens/expenses_screen.dart
+++ lib/screens/expenses_screen.dart
@@ -1,4 +1,7 @@
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:flutter/material.dart';
+
+import '../models/expense.dart';
 
 class ExpensesScreen extends StatefulWidget {
   const ExpensesScreen({super.key});
@@ -8,13 +11,32 @@ class ExpensesScreen extends StatefulWidget {
 }
 
 class _ExpensesScreenState extends State<ExpensesScreen> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: "Flutter Course",
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: "Cinema",
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          Text("The chart"),
-          Text("Expenses list..."),
+          const Text("The chart"),
+          Expanded(
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+            ),
+          )
         ],
       ),
     );
