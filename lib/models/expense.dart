a3ed1a66ba999b004261d40caaca097a80f35c04
Angelo Filho <angelo.filho06@hotmail.com>
2023-07-03 15:40:16 -0300

    feat: add formatted values to expense model
    - Date format
    - Amount format

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,4 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
+
+final formatter = DateFormat.yMd("pt_BR");
+final currencyFormatter = NumberFormat.currency(
+  decimalDigits: 2,
+  locale: "pt_BR",
+  symbol: "R\$",
+);
 
 final uuid = Uuid();
 
@@ -8,6 +17,13 @@ enum Category {
   leisure,
   work,
 }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
 
 class Expense {
   Expense({
@@ -22,4 +38,12 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
+
+  String get formattedAmount {
+    return currencyFormatter.format(amount);
+  }
 }
