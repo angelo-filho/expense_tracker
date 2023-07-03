5db41c195845ce9e97bd9539ce7efa23667ddc35
Angelo Filho <angelo.filho06@hotmail.com>
2023-07-03 15:51:57 -0300

    feat: define pt-BR as default locale

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:expense_tracker/screens/expenses_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() {
+  initializeDateFormatting("pt_BR", null);
+
   runApp(
     const MaterialApp(
       home: ExpensesScreen(),
+      locale: Locale("pt", "BR"),
     ),
   );
 }
