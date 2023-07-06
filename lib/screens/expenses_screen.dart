ad5f3bea5f84df537909deb7db36a6380d3b4d46
Angelo Filho <angelo.filho06@hotmail.com>
2023-07-06 12:58:07 -0300

    feat: add padding and full height to new expense modal

--- lib/screens/expenses_screen.dart
+++ lib/screens/expenses_screen.dart
@@ -29,6 +29,7 @@ class _ExpensesScreenState extends State<ExpensesScreen> {
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
+      isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpenseModal(onAddExpense: _addExpense),
     );
--- lib/widgets/new_expense_modal.dart
+++ lib/widgets/new_expense_modal.dart
@@ -80,7 +80,7 @@ class _NewExpenseModalState extends State<NewExpenseModal> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16),
+      padding: const EdgeInsets.fromLTRB(16, 48, 16, 16),
       child: Column(
         children: [
           TextField(
