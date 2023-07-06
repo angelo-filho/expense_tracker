235a81339a45b168115c5871daa752507d4ce7f6
Angelo Filho <angelo.filho06@hotmail.com>
2023-07-06 12:09:30 -0300

    feat: add a dropdown button to select the category of an expense

--- lib/widgets/new_expense_modal.dart
+++ lib/widgets/new_expense_modal.dart
@@ -12,6 +12,7 @@ class _NewExpenseModalState extends State<NewExpenseModal> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
 
   void _presentDatePicker() async {
     final now = DateTime.now();
@@ -80,13 +81,33 @@ class _NewExpenseModalState extends State<NewExpenseModal> {
           ),
           Row(
             children: [
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(
+                          category.name.toUpperCase(),
+                        ),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  if (value == null) return;
+
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                },
+              ),
+              const Spacer(),
               TextButton(
                 onPressed: () {
                   Navigator.pop(context);
                 },
                 child: const Text('Cancel'),
               ),
-              const Spacer(),
               ElevatedButton(
                 onPressed: () {
                   print(_titleController.text);
